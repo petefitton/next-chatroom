65cb40c2c72c15ef834bbf192e1c363719fcf2ec
Pete Fitton <petefitton@gmail.com>
2025-10-21 16:33:40 -0600

    Add async to app/chat/page and getRoomIds

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,8 +1,8 @@
 import { getRoomIds } from "@/app/lib/data";
 import ChatroomItem from "../ui/chat/ChatroomItem";
 
-export default function Chat() {
-  const rooms = getRoomIds();
+export default async function Chat() {
+  const rooms = await getRoomIds();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,5 +1,5 @@
 'use server'
 
-export function getRoomIds() {
+export async function getRoomIds() {
     return [{id: "1", name: "Chatroom name"}, {id: "2", name: "Let's talk about the weather!"}]
 }
