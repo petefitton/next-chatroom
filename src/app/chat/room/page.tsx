4274184d1a1c68de60d7a84b4646b44e35f17d4d
Pete Fitton <petefitton@gmail.com>
2025-10-21 16:05:18 -0600

    Stub a few pages

--- src/app/chat/room/page.tsx
+++ src/app/chat/room/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Room() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Welcome to Chatty!
+        Here is the chatroom you selected
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
