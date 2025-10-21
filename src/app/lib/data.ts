62a73df1cfa9d9b49288483a8b42c67486ce3660
Pete Fitton <petefitton@gmail.com>
2025-10-21 16:32:19 -0600

    Update data.ts

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,3 +1,5 @@
+'use server'
+
 export function getRoomIds() {
-    return [{id: "1", name: "Chatroom name"}]
+    return [{id: "1", name: "Chatroom name"}, {id: "2", name: "Let's talk about the weather!"}]
 }
